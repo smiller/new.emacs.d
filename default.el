6ef9ff873d8805a59f89cd8684aba0d47f20a8b7
smiller <sean@seanmiller.ca>
2013-08-08 23:07:07 -0400

    Starter default.el from nicferrier's emacsbites

---
+++ default.el
@@ -0,0 +1,10 @@
+;; call from a zsh function like
+;; @ne() { mkdir "$@"; HOME=./"$@" emacs -l ~/code/new.emacs.d/default.el }
+;; initial version from nicferrier's emacsbites
+(package-refresh-contents)
+(customize-set-variable
+  'package-archives
+  (add-to-list 'package-archives (cons "marmalade" "http://marmalade-repo.org/packages/")))
+(customize-set-variable 'show-paren-mode t)
+(customize-save-customized)
+(package-refresh-contents)
